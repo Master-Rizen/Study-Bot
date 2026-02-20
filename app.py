5f6870da7fc4d9c448447a18b75c7c51e1d9de37
Rizen <149863200+Master-Rizen@users.noreply.github.com>
2026-02-21 00:15:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+import datetime
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from pymongo import MongoClient
+from datetime import datetime
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+mongo_uri = os.getenv("MONGODB_URI")
+
+client = MongoClient(mongo_uri)
+db = client["Chatbot"]
+collection = db["users"]
+
+app = FastAPI()
+
+class ChatRequest(BaseModel):
+    user_id: str
+    question: str
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a study bot designed to help learners understand and retain knowledge. Provide clear, accurate, and practical explanations of topics step by step, use tables or bullet points for comparisons, and include examples to make concepts easier to grasp. When asked for study material, generate quizzes, flashcards, or practice questions. Keep your tone professional yet supportive, focus on general educational guidance, and encourage effective study habits while clarifying that deeper subject-specific advice should be sought from experts if needed."),
+        ("placeholder", "{history}"),
+        ("user", "{question}")
+    ]
+)
+
+llm = ChatGroq(api_key = groq_api_key, model="openai/gpt-oss-20b")
+chain = prompt | llm
+
+def get_history(user_id):
+    chats = collection.find({"user_id": user_id}).sort("timestamp", 1)
+    history = []
+
+    for chat in chats:
+        history.append((chat["role"], chat["message"]))
+    return history
+
+@app.get("/") 
+def home():
+    return {"message": "Welcome to the Study Bot!"}
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    history = get_history(request.user_id)
+    response = chain.invoke({"history": history, "question": request.question})
+
+    collection.insert_one({
+        "user_id": request.user_id,
+        "role": "user",
+        "message": request.question,
+        "timestamp": datetime.utcnow()
+    })
+
+    collection.insert_one({
+        "user_id": request.user_id,
+        "role": "assistant",
+        "message": response.content,
+        "timestamp": datetime.utcnow()
+    })
+
+    return {"response" : response.content}
